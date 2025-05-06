a565c07659b6998f3bce8a0a222498ed71e6e56d
pakhrom <ro.pakhomov@gmail.com>
2025-05-06 13:57:26 +0700

    fix(web): trim string form fields before validation

--- web/src/lib/schemas/subject.ts
+++ web/src/lib/schemas/subject.ts
@@ -1,10 +1,11 @@
 import { z } from 'zod';
 
 export const subjectWrite = z.object({
-	name: z.string({ required_error: 'Введите имя' }).min(1, { message: 'Введите имя' }),
+	name: z.string({ required_error: 'Введите имя' }).trim().min(1, { message: 'Введите имя' }),
 	shortName: z
 		.string()
 		.max(12, { message: 'Короткое название предмета не должно быть длиной более 12 символов' })
+		.trim()
 		.optional(),
 	optional: z.boolean().default(false)
 });
