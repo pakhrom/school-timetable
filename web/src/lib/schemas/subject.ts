df52d475a7e1b831c602171fe5425e67eac9c696
pakhrom <ro.pakhomov@gmail.com>
2025-05-06 13:58:04 +0700

    feat(web): change subject.shortName max length to 10

--- web/src/lib/schemas/subject.ts
+++ web/src/lib/schemas/subject.ts
@@ -4,8 +4,8 @@ export const subjectWrite = z.object({
 	name: z.string({ required_error: 'Введите имя' }).trim().min(1, { message: 'Введите имя' }),
 	shortName: z
 		.string()
-		.max(12, { message: 'Короткое название предмета не должно быть длиной более 12 символов' })
 		.trim()
+		.max(10, { message: 'Короткое название предмета не должно быть длиной более 10 символов' })
 		.optional(),
 	optional: z.boolean().default(false)
 });
