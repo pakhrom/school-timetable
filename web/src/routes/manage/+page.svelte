395a7658700941d09eabebcac8981bc889ef719f
pakhrom <ro.pakhomov@gmail.com>
2025-05-07 23:31:59 +0700

    feat(web): add empty call schedules tab

--- web/src/routes/manage/+page.svelte
+++ web/src/routes/manage/+page.svelte
@@ -6,6 +6,7 @@
 
 	let replacementsTab: HTMLButtonElement;
 	let timetablesTab: HTMLButtonElement;
+	let callScheduleTab: HTMLButtonElement;
 	let groupsTab: HTMLButtonElement;
 	let subjectsTab: HTMLButtonElement;
 	let teachersTab: HTMLButtonElement;
@@ -19,6 +20,9 @@
 				break;
 			case 'timetables':
 				timetablesTab.scrollIntoView({ inline: 'center' });
+				break;
+			case 'callSchedule':
+				callScheduleTab.scrollIntoView({ inline: 'center' });
 				break;
 			case 'groups':
 				groupsTab.scrollIntoView({ inline: 'center' });
@@ -488,6 +492,31 @@
 		Расписания
 	</button>
 	<button
+		class={['secondary', 'tab', currentTab.tab === 'callSchedule' ? '' : 'outline']}
+		bind:this={callScheduleTab}
+		onclick={() => {
+			currentTab.tab = 'callSchedule';
+			callScheduleTab.scrollIntoView({ inline: 'center', behavior: 'smooth' });
+		}}
+	>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="20"
+			height="20"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			class="icon icon-tabler icons-tabler-outline icon-tabler-bell"
+			><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
+				d="M10 5a2 2 0 1 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6"
+			/><path d="M9 17v1a3 3 0 0 0 6 0v-1" /></svg
+		>
+		Звонки
+	</button>
+	<button
 		class={['secondary', 'tab', currentTab.tab === 'groups' ? '' : 'outline']}
 		bind:this={groupsTab}
 		onclick={() => {
@@ -623,6 +652,8 @@
 			</div>
 		</article>
 	{/each}
+{:else if currentTab.tab === 'callSchedule'}
+	<!-- CALL SCHEDULE -->
 {:else if currentTab.tab === 'groups'}
 	<!-- GROUPS TAB -->
 	<div class="filter">
